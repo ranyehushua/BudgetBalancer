c5a4664ae256fa4c6ee06bd0875b9ac6dea48955
ranyehushua <ranyehushua@gmail.com>
2017-01-19 15:13:54 -0700

    Linked the input values in AddItem to its state

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -4,24 +4,32 @@ const AddItem = React.createClass({
   propTypes: {
     onClick: PropTypes.func.isRequired
   },
+  getInitialState () {
+    return {text: '', amount: ''}
+  },
+  handleTextChange(e) {
+    this.setState({text: e.target.value})
+  },
+  handleAmountChange(e) {
+    this.setState({amount: e.target.value})
+  },
   render () {
     return (
       <form className="form-inline" onSubmit={(e) => {
           e.preventDefault();
           //will need to add security validation for text input
-          if (AddItem.text.value && parseFloat(AddItem.amount.value) !== NaN) {
-            this.props.onClick(AddItem.text.value, parseFloat(AddItem.amount.value));
-            AddItem.text.value = '';
-            AddItem.amount.value = '';
+          if (this.state.text && parseFloat(this.state.amount) !== NaN) {
+            this.props.onClick(this.state.text, parseFloat(this.state.amount));
+            this.setState({text: '', amount: ''});
           }
         }}>
         <div className="form-group">
           <label className="sr-only" htmlFor="text">Item Description</label>
-          <input type="text" className="form-control" id="text" placeholder="Item Description" onChange={() => AddItem.text = this.value} />
+          <input type="text" className="form-control" id="text" placeholder="Item Description" value={this.state.text} onChange={this.handleTextChange} />
         </div>
         <div className="form-group">
           <label className="sr-only" htmlFor="amount">Amount</label>
-          <input type="number" className="form-control" id="amount" placeholder="Amount" onChange={() => AddItem.amount = this.value} />
+          <input type="number" className="form-control" id="amount" placeholder="Amount" value={this.state.amount} onChange={this.handleAmountChange} />
         </div>
         <div className="form-group">
           <button className="btn btn-success" type="submit">Add Item</button>
