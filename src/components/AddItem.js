a1fa1d15cec0903fd88d521ee685feb316ffa57e
ranyehushua <ranyehushua@gmail.com>
2017-01-19 14:45:05 -0700

    Updated AddItem to be stateful component since it is a form

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,28 +1,34 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
 
-const AddItem = (props) => (
-    <form className="form-inline" onSubmit={(e) => {
-        e.preventDefault();
-        props.onClick(AddItem.text.value, parseInt(AddItem.amount.value));
-        AddItem.text.value = '';
-        AddItem.amount.value = '';
-      }}>
-      <div className="form-group">
-        <label className="sr-only" htmlFor="text">Email address</label>
-        <input type="text" className="form-control" id="text" placeholder="Item Description" ref={(text) => AddItem.text = text} />
-      </div>
-      <div className="form-group">
-        <label className="sr-only" htmlFor="amount">Email address</label>
-        <input type="number" className="form-control" id="amount" placeholder="Amount" ref={(amount) => AddItem.amount = amount} />
-      </div>
-      <div className="form-group">
-        <button className="btn btn-success" type="submit">Add Item</button>
-      </div>
-    </form>
-)
-
-AddItem.propTypes = {
-  onClick: PropTypes.func.isRequired
-}
+const AddItem = React.createClass({
+  propTypes: {
+    onClick: PropTypes.func.isRequired
+  },
+  render () {
+    return (
+      <form className="form-inline" onSubmit={(e) => {
+          e.preventDefault();
+          //will need to add security validation for text input
+          if (AddItem.text.value && parseFloat(AddItem.amount.value) !== NaN) {
+            this.props.onClick(AddItem.text.value, parseFloat(AddItem.amount.value));
+            AddItem.text.value = '';
+            AddItem.amount.value = '';
+          }
+        }}>
+        <div className="form-group">
+          <label className="sr-only" htmlFor="text">Item Description</label>
+          <input type="text" className="form-control" id="text" placeholder="Item Description" onChange={() => AddItem.text = this.value} />
+        </div>
+        <div className="form-group">
+          <label className="sr-only" htmlFor="amount">Amount</label>
+          <input type="number" className="form-control" id="amount" placeholder="Amount" onChange={() => AddItem.amount = this.value} />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-success" type="submit">Add Item</button>
+        </div>
+      </form>
+    )
+  }
+})
 
 export default AddItem;
