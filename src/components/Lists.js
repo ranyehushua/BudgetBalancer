d2d2eb45dd1491bff9507a10e0746ceabf317215
ranyehushua <ranyehushua@gmail.com>
2017-01-19 23:57:40 -0700

    updated list headers

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -5,11 +5,11 @@ import List from './List';
 const Lists = (props) => {
   return (
     <div>
-    <h2>Income:</h2>
+    <h2>Monthly Income:</h2>
       <List items={props.income.items} clickRemove={props.removeIncome} />
       <p>Total Income: {props.income.total}</p>
     <AddItem onClick={props.addIncome}/>
-      <h2>Expenses:</h2>
+      <h2>Monthly Expenses:</h2>
         <List items={props.expense.items} clickRemove={props.removeExpense} />
         <p>Total Income: {props.expense.total}</p>
       <AddItem onClick={props.addExpense}/>
