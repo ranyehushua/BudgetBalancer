a717e11df8fce10cd3ba4beba2b70f9266078f3b
ranyehushua <ranyehushua@gmail.com>
2017-01-26 13:19:42 -0700

    Added $ symbol to LineItem component

--- src/components/LineItem.js
+++ src/components/LineItem.js
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react';
 const LineItem = (props) => {
   return (
     <li>
-      <p>{props.desc + ": " + props.amount}</p>
+      <p>{props.desc + ": $" + props.amount}</p>
       <button className="btn btn-danger" onClick={props.onClick}>Remove item</button>
     </li>
   )
