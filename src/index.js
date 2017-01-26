a655d9e5cffdc717150bd81c54dcf39516b097af
ranyehushua <ranyehushua@gmail.com>
2017-01-26 15:54:22 -0700

    Added React Router and Main component to application root

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import './index.css';
 import { testIncome } from './reducer/income';
 import { testExpense } from './reducer/expense';
+import { testUser } from './reducer/user';
 import reducer from './reducer';
 import { createStore } from 'redux';
 import IncomeExpenseLists from './containers/IncomeExpenseLists';
+import IncomeExpensePrompts from './containers/IncomeExpensePrompts';
+import MainContainer from './containers/MainContainer';
 import { loadState, saveState } from './utils/localStorage';
+import { addIncome } from './actions';
+import { syncHistoryWithStore } from 'react-router-redux';
 
 const persistedState = loadState();
 const store = createStore(reducer, persistedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-const App = () => (
-  <div>
-    <IncomeExpenseLists />
-  </div>
-)
 store.subscribe(() => {
   saveState({
     income: store.getState().income,
@@ -24,12 +25,27 @@ store.subscribe(() => {
   });
 });
 
+// localStorage.removeItem('budgetCalcState');
+// store.dispatch(addIncome('poop', 100));
+
+// const App = () => {
+//   return store.getState().income.items.length > 0 || store.getState().expense.items.length > 0
+//     ? <div><IncomeExpenseLists /></div>
+//     : <div><IncomeExpensePrompts /></div>
+// }
+
+const history = syncHistoryWithStore(browserHistory, store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path='/' component={MainContainer}/>
+
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
 
 testIncome();
 testExpense();
+testUser();
