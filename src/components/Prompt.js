dfe88bd45765ee6ca9cc2d94880f7ba93e692146
ranyehushua <ranyehushua@gmail.com>
2017-01-27 13:47:21 -0700

    Updated Prompt component styling

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -14,26 +14,31 @@ const Prompt = React.createClass({
   handleAmountChange(e) {
     this.setState({amount: e.target.value})
   },
+  nextView() {
+    browserHistory.push('/' + this.props.next);
+  },
   render () {
     return (
-      <div>
-        <p>{this.props.prompt}</p>
+      <div className='text-center'>
+        <h3>{this.props.prompt}</h3>
           <form className="form-inline" onSubmit={(e) => {
               e.preventDefault();
               //will need to add security validation for text input
               if (parseFloat(this.state.amount)) {
                 this.props.addItem(this.props.desc, parseFloat(this.state.amount));
                 this.setState({amount: ''});
-                browserHistory.push('/' + this.props.next);
+                this.nextView();
               }
             }}>
             <div className="form-group">
               <label className="sr-only" htmlFor="amount">Amount</label>
-              <input type="number" className="form-control" id="amount" placeholder="Amount" value={this.state.amount} onChange={this.handleAmountChange} />
+              <div className="input-group">
+                <div className="input-group-addon">$</div>
+                <input type="number" className="form-control" id="amount" placeholder="Amount" value={this.state.amount} onChange={this.handleAmountChange} />
+              </div>
             </div>
-            <div className="form-group">
-              <button className="btn btn-success" type="submit">Add Item</button>
-            </div>
+            <button type="submit" className="btn btn-success" style={{margin: '10px'}}>Add Item</button>
+            <button type="button" className="btn btn-default" onClick={this.nextView}>Skip Item</button>
           </form>
       </div>
     )
