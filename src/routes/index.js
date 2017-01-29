46728885bd7380f2572ff94c1976cf13f2bb2e38
ranyehushua <ranyehushua@gmail.com>
2017-01-28 19:44:05 -0700

    Looping through prompts array to generate prompt routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,31 @@
 import MainContainer from '../containers/MainContainer';
-import WorkingIncome from '../components/Prompts/Income/WorkingIncome';
-import SpouseWorkingIncome from '../components/Prompts/Income/SpouseWorkingIncome';
-import InvestmentIncome from '../components/Prompts/Income/InvestmentIncome';
 import TutorialContainer from '../containers/TutorialContainer';
 import BudgetList from '../containers/BudgetList';
 import App from '../components/App';
 import { Route, IndexRoute } from 'react-router';
 import React from 'react';
 
+import IncomePrompt from '../containers/IncomePrompt';
+import { incomePrompts } from './prompts';
+
+//IncomePrompt component expects prompt, desc, next props
+const IncomeRoutes = [];
+incomePrompts.forEach((item, i) => {
+  const Component = (props) => {
+    return (
+      <IncomePrompt prompt={item.prompt} desc={item.desc} next={i === incomePrompts.length - 1 ? 'BudgetList' : incomePrompts[i+1].desc.split(' ').join('')} key={i.toString()}/>
+    )
+  }
+  const route = (<Route path={'/' + item.desc.split(' ').join('')} component={Component} key={i.toString()} />);
+  IncomeRoutes.push(route);
+});
+
 const routes = (
   <div>
     <Route path='/' component={App} >
       <IndexRoute component={MainContainer} />
+      {IncomeRoutes}
       <Route path='/BudgetList' component={BudgetList} />
-      <Route path='/WorkingIncome' component={WorkingIncome} />
-      <Route path='/SpouseWorkingIncome' component={SpouseWorkingIncome} />
-      <Route path='/InvestmentIncome' component={InvestmentIncome} />
       <Route path='/Tutorial' component={TutorialContainer} />
     </Route>
   </div>
