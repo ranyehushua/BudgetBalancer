f2911476688e9ccecf0ac6fae8365df120a709a5
ranyehushua <ranyehushua@gmail.com>
2017-01-28 20:10:35 -0700

    Main now stateful, for redirect if returning user
    Using componentDidMount to check returning user and nav to List

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,47 @@
 import React, { PropTypes } from 'react';
 import {browserHistory} from 'react-router';
 import NewUser from './NewUser';
+import BudgetList from '../containers/BudgetList';
 
 const goToList = () => {
   browserHistory.push('/BudgetList');
 }
 
-const Main = (props) => {
-  return (
-    <div>
-      {props.income.idCounter > 0 || props.expense.idCounter > 10000
-        ? {goToList}
-        : <NewUser user={props.user} addUser={props.addUser} /> //Replace with link to Add new user component
-      }
-    </div>
-  )
-}
+const Main = React.createClass({
+  propTypes: {
+    income: PropTypes.object.isRequired,
+    expense: PropTypes.object.isRequired,
+    user: PropTypes.string.isRequired,
+    addUser: PropTypes.func.isRequired
+  },
+  componentDidMount() {
+    if (this.props.idCounter > 0 || this.props.expense.idCounter > 10000) browserHistory.push('/BudgetList');
+  },
+  render () {
+    return (
+        <NewUser user={this.props.user} addUser={this.props.addUser} />
+    )
+  }
+});
 
-Main.propTypes = {
-   income: PropTypes.object.isRequired,
-   expense: PropTypes.object.isRequired,
-   user: PropTypes.string.isRequired,
-   addUser: PropTypes.func.isRequired
-}
-
-export default Main;
+export default Main
+//
+// const Main = (props) => {
+//   return (
+//     <div>
+//       {props.income.idCounter > 0 || props.expense.idCounter > 10000
+//         ? <BudgetList />
+//         : <NewUser user={props.user} addUser={props.addUser} /> //Replace with link to Add new user component
+//       }
+//     </div>
+//   )
+// }
+//
+// Main.propTypes = {
+//    income: PropTypes.object.isRequired,
+//    expense: PropTypes.object.isRequired,
+//    user: PropTypes.string.isRequired,
+//    addUser: PropTypes.func.isRequired
+// }
+//
+// export default Main;
