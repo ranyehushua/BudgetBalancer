b3e54a09b4ad1b96b01580202d0aeebfe60065b7
ranyehushua <ranyehushua@gmail.com>
2017-01-30 15:04:38 -0700

    LineItem now createClass rather than stateless component
    Needed in order to add edit line item functionality

--- src/components/LineItem.js
+++ src/components/LineItem.js
@@ -1,20 +1,21 @@
 import React, { PropTypes } from 'react';
 
-const LineItem = (props) => {
-  return (
-    <li>
-      {props.desc + ": $" + props.amount}
-      <button className="btn btn-danger" onClick={props.remove} style={{padding: '0 3px'}}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
-    </li>
-  )
-}
-
-LineItem.propTypes = {
-  desc: PropTypes.string.isRequired,
-  amount: PropTypes.number.isRequired,
-  id: PropTypes.number.isRequired,
-  remove: PropTypes.func.isRequired,
-  edit: PropTypes.func.isRequired
-}
+const LineItem = React.createClass({
+  propTypes: {
+    desc: PropTypes.string.isRequired,
+    amount: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
+    remove: PropTypes.func.isRequired,
+    edit: PropTypes.func.isRequired
+  },
+  render () {
+    return (
+      <li>
+        {this.props.desc + ": $" + this.props.amount}
+        <button className="btn btn-danger" onClick={this.props.remove} style={{padding: '0 3px'}}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+      </li>
+    )
+  }
+})
 
 export default LineItem;
