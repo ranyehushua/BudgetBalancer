b1c10d57d0197b428acd7d67000c5c47232ec81b
ranyehushua <ranyehushua@gmail.com>
2017-01-30 14:27:50 -0700

    Updated text styling in Lists

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -7,23 +7,23 @@ const Lists = (props) => {
     <div>
       <h1 className="text-center">Welcome {props.user}!</h1>
       {props.income.items.length > 0
-        ? <div><h2>Monthly Income:</h2>
+        ? <div><h3>Monthly Income:</h3>
         <List items={props.income.items} clickRemove={props.removeIncome} />
-        <p>Total Monthly Income: ${props.income.total}</p></div>
+        <h4>Total Monthly Income: ${props.income.total}</h4></div>
         : null}
 
       <AddItem onClick={props.addIncome} className='text-center'/>
       {props.expense.essential.length > 0
-        ? <div><h2>Monthly Essential Expenses:</h2>
+        ? <div><h3>Monthly Essential Expenses:</h3>
         <List items={props.expense.essential} clickRemove={props.removeExpense} /></div>
         : null}
 
       {props.expense.discretionary.length > 0
-        ? <div><h2>Monthly Discretionary Expenses:</h2>
+        ? <div><h3>Monthly Discretionary Expenses:</h3>
       <List items={props.expense.discretionary} clickRemove={props.removeExpense} /></div>
         : null}
       {props.expense.discretionary.length > 0 || props.expense.essential.length > 0
-        ? <p>Total Monthly Expenses: ${props.expense.total}</p>
+        ? <h4>Total Monthly Expenses: ${props.expense.total}</h4>
         : null}
       <AddItem onClick={props.addExpense} expense />
     </div>
