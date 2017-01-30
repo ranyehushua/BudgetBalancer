0be29b8a0eefcbc8f6cad9d2094acbacf361f54b
ranyehushua <ranyehushua@gmail.com>
2017-01-30 12:59:18 -0700

    Added conditional rendering to Lists component

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -6,15 +6,25 @@ const Lists = (props) => {
   return (
     <div>
       <h1 className="text-center">Welcome {props.user}!</h1>
-      <h2>Monthly Income:</h2>
-      <List items={props.income.items} clickRemove={props.removeIncome} />
-      <p>Total Monthly Income: ${props.income.total}</p>
-      <AddItem onClick={props.addIncome}/>
-      <h2>Monthly Essential Expenses:</h2>
-      <List items={props.expense.essential} clickRemove={props.removeExpense} />
-      <h2>Monthly Discretionary Expenses:</h2>
-      <List items={props.expense.discretionary} clickRemove={props.removeExpense} />
-      <p>Total Monthly Expenses: ${props.expense.total}</p>
+      {props.income.items.length > 0
+        ? <div><h2>Monthly Income:</h2>
+        <List items={props.income.items} clickRemove={props.removeIncome} />
+        <p>Total Monthly Income: ${props.income.total}</p></div>
+        : null}
+
+      <AddItem onClick={props.addIncome} className='text-center'/>
+      {props.expense.essential.length > 0
+        ? <div><h2>Monthly Essential Expenses:</h2>
+        <List items={props.expense.essential} clickRemove={props.removeExpense} /></div>
+        : null}
+
+      {props.expense.discretionary.length > 0
+        ? <div><h2>Monthly Discretionary Expenses:</h2>
+      <List items={props.expense.discretionary} clickRemove={props.removeExpense} /></div>
+        : null}
+      {props.expense.discretionary.length > 0 || props.expense.essential.length > 0
+        ? <p>Total Monthly Expenses: ${props.expense.total}</p>
+        : null}
       <AddItem onClick={props.addExpense} expense />
     </div>
   )
