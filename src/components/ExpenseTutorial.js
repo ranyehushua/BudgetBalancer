31d48e7a2b7cac4bff9ba24c40cd03cc87e51ef7
ranyehushua <ranyehushua@gmail.com>
2017-01-30 09:55:31 -0700

    Added Expense Tutorial

---
+++ src/components/ExpenseTutorial.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {browserHistory} from 'react-router';
+
+const ExpenseTutorial = (props) => {
+  return (
+    <div>
+      <h1>Great work so far!</h1>
+      <p>Don't worry, if you have more income sources to add to the list, you will have an opportunity to add them shortly.</p>
+      <p>For now, let's go through a few expenses. This will work just like the income prompts, we will start with the essentials and later you will have the opportunity to add more discretionary or essential expenses.</p>
+      <h4>Ready to get started? <button className="btn btn-success" type="button" onClick={() => browserHistory.push('/HousingExpense')}>Let's Go!</button></h4>
+    </div>
+  )
+}
+
+export default ExpenseTutorial;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import MainContainer from '../containers/MainContainer';
 import TutorialContainer from '../containers/TutorialContainer';
+import ExpenseTutorial from '../components/ExpenseTutorial';
 import BudgetList from '../containers/BudgetList';
 import App from '../components/App';
 import { Route, IndexRoute } from 'react-router';
 import React from 'react';
 import incomeRoutes from './incomeRoutes';
+import expenseRoutes from './expenseRoutes';
 
 
 const routes = (
@@ -12,6 +14,8 @@ const routes = (
     <Route path='/' component={App} >
       <IndexRoute component={MainContainer} />
       {incomeRoutes}
+      <Route path='/ExpenseTutorial' component={ExpenseTutorial} />
+      {expenseRoutes}
       <Route path='/BudgetList' component={BudgetList} />
       <Route path='/Tutorial' component={TutorialContainer} />
     </Route>
