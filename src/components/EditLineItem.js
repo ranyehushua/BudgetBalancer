21edc203e7a666ba6f85dbf7261dbbaec82d3464
ranyehushua <ranyehushua@gmail.com>
2017-01-30 23:09:19 -0700

    EditLineItem now functional - also adjust list styling

--- src/components/EditLineItem.js
+++ src/components/EditLineItem.js
@@ -1,18 +1,59 @@
 import React, { PropTypes } from 'react';
 
-const EditLineItem = (props) => {
-  return (
-    <div>
-      Working
-    </div>
-  )
+const style = {
+  button: {
+    padding: '2px 5px',
+    float: 'right'
+  },
+  desc: {
+    maxWidth: '60%',
+    marginRight: '5px'
+  },
+  amount: {
+    maxWidth: '25%'
+  }
 }
 
-EditLineItem.PropTypes = {
-  edit: PropTypes.func.isRequired,
-  desc: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
-  amount: PropTypes.number.isRequired
-}
+const EditLineItem = React.createClass({
+  propTypes: {
+    edit: PropTypes.func.isRequired,
+    desc: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    amount: PropTypes.number.isRequired
+  },
+  getInitialState() {
+    return {
+      desc: this.props.desc,
+      amount: this.props.amount
+    }
+  },
+  handleTextChange(e) {
+    this.setState({
+      desc: e.target.value
+    });
+  },
+  handleAmountChange(e) {
+    this.setState({
+      amount: e.target.value
+    })
+  },
+  handleSubmit() {
+    this.props.edit(this.props.id, this.state.desc, parseFloat(this.state.amount));
+  },
+  render () {
+    return (
+      <div>
+        <input type="text" value={this.state.desc} onChange={this.handleTextChange} style={style.desc} />
+        <input type="number" value={this.state.amount} onChange={this.handleAmountChange} style={style.amount} />
+          <button type="button" className="btn btn-success" aria-label="Left Align" onClick={this.handleSubmit} style={style.button}>
+            <span className="glyphicon glyphicon-ok" aria-hidden="true"></span>
+          </button>
+      </div>
+    )
+  }
+})
+
+
+
 
 export default EditLineItem;
--- src/components/LineItem.js
+++ src/components/LineItem.js
@@ -1,6 +1,21 @@
 import React, { PropTypes } from 'react';
 import EditLineItem from './EditLineItem';
 
+const style={
+  li: {
+    border: 'black solid 1px',
+    margin: '3px 0',
+    padding: '5px'
+  },
+  button: {
+    padding: '1px 3px',
+    float: 'right',
+    fontSize: '12px',
+    marginLeft: '3px'
+  }
+
+}
+
 const LineItem = React.createClass({
   propTypes: {
     desc: PropTypes.string.isRequired,
@@ -14,15 +29,21 @@ const LineItem = React.createClass({
       editting: false
     }
   },
+  submitEdit(id, desc, amount) {
+    this.props.edit(id, desc, amount);
+    this.setState({
+      editting: false
+    });
+  },
   render () {
     return (
-      <li>
+      <li style={style.li}>
         {this.state.editting === true
-          ? <EditLineItem edit={this.props.edit} id={this.props.id} desc={this.props.desc} amount={this.props.amount} />
+          ? <EditLineItem edit={this.submitEdit} id={this.props.id} desc={this.props.desc} amount={this.props.amount} />
           : <div>
               {this.props.desc + ": $" + this.props.amount}
-              <button className="btn btn-danger" onClick={this.props.remove} style={{padding: '0 3px'}}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
-              <button className="btn btn-default" onClick={() => {this.setState({editting: !this.state.editting})}} style={{padding: '0 3px'}}><span className="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
+              <button className="btn btn-danger" onClick={this.props.remove} style={style.button}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+              <button className="btn btn-default" onClick={() => {this.setState({editting: !this.state.editting})}} style={style.button}><span className="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
             </div>
         }
 
