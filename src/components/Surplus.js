53e80fb349399c390f64f99dc66c80ff4891353d
ranyehushua <ranyehushua@gmail.com>
2017-01-31 15:49:36 -0700

    Updated language in Surplus banner

--- src/components/Surplus.js
+++ src/components/Surplus.js
@@ -13,7 +13,7 @@ const style = {
 const Surplus = (props) => {
   return (
     <div style={style} className="text-center">
-      You have a surplus of ${props.amount} in your budget! Scroll below and let's allocate some savings!
+      You currently have a surplus of ${props.amount} in your budget! Scroll below to adjust your essential and discretionary expenses or, better yet, let's allocate some savings!
     </div>
   )
 }
