47a6284bf261bbc8e06b6db48d486ca5e3d26595
ranyehushua <ranyehushua@gmail.com>
2017-01-31 11:16:06 -0700

    Adjust format condition rendering blocks in Lists readability

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -6,25 +6,31 @@ const Lists = (props) => {
   return (
     <div>
       <h1 className="text-center">Welcome {props.user}!</h1>
-      {props.income.items.length > 0
-        ? <div><h3>Monthly Income:</h3>
-        <List items={props.income.items} clickRemove={props.removeIncome} clickEdit={props.editIncome} />
-        <h4>Total Monthly Income: ${props.income.total}</h4></div>
-        : null}
-
+      {
+        props.income.items.length > 0
+          ? <div><h3>Monthly Income:</h3>
+          <List items={props.income.items} clickRemove={props.removeIncome} clickEdit={props.editIncome} />
+          <h4>Total Monthly Income: ${props.income.total}</h4></div>
+          : null
+      }
       <AddItem onClick={props.addIncome} className='text-center'/>
-      {props.expense.essential.length > 0
-        ? <div><h3>Monthly Essential Expenses:</h3>
-        <List items={props.expense.essential} clickRemove={props.removeExpense} clickEdit={props.editExpense} /></div>
-        : null}
-
-      {props.expense.discretionary.length > 0
-        ? <div><h3>Monthly Discretionary Expenses:</h3>
-      <List items={props.expense.discretionary} clickRemove={props.removeExpense} clickEdit={props.editExpense} /></div>
-        : null}
-      {props.expense.discretionary.length > 0 || props.expense.essential.length > 0
-        ? <h4>Total Monthly Expenses: ${props.expense.total}</h4>
-        : null}
+      {
+        props.expense.essential.length > 0
+          ? <div><h3>Monthly Essential Expenses:</h3>
+          <List items={props.expense.essential} clickRemove={props.removeExpense} clickEdit={props.editExpense} /></div>
+          : null
+      }
+      {
+        props.expense.discretionary.length > 0
+          ? <div><h3>Monthly Discretionary Expenses:</h3>
+            <List items={props.expense.discretionary} clickRemove={props.removeExpense} clickEdit={props.editExpense} /></div>
+          : null
+      }
+      {
+        props.expense.discretionary.length > 0 || props.expense.essential.length > 0
+          ? <h4>Total Monthly Expenses: ${props.expense.total}</h4>
+          : null
+      }
       <AddItem onClick={props.addExpense} expense />
     </div>
   )
