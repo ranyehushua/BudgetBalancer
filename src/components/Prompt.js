d48602f47530a13d483623716a4e3ffd81900d6c
ranyehushua <ranyehushua@gmail.com>
2017-02-01 11:13:42 -0700

    Updated Prompt element with new TransparentContainer wrapper

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import {hashHistory} from 'react-router';
 import CurrencyInput from './CurrencyInput';
+import TransparentContainer from './TransparentContainer';
 
 const Prompt = React.createClass({
   propTypes: {
@@ -21,21 +22,25 @@ const Prompt = React.createClass({
   },
   render () {
     return (
-      <div className='text-center'>
-        <h3>{this.props.prompt}</h3>
-          <form className="form-inline" onSubmit={(e) => {
-              e.preventDefault();
-              //will need to add security validation for text input
-              if (parseFloat(this.state.amount)) {
-                this.props.addItem(this.props.desc, parseFloat(this.state.amount), this.props.essential);
-                this.setState({amount: ''});
-                this.nextView();
-              }
-            }}>
-            <CurrencyInput value={this.state.amount} onChange={this.handleAmountChange} />
-            <button type="submit" className="btn btn-success" style={{margin: '10px'}}>Add Item</button>
-            <button type="button" className="btn btn-default" onClick={this.nextView}>Skip Item</button>
-          </form>
+      <div className='prompt'>
+        <TransparentContainer>
+          <div className='text-center'>
+            <h3>{this.props.prompt}</h3>
+              <form className="form-inline" onSubmit={(e) => {
+                  e.preventDefault();
+                  //will need to add security validation for text input
+                  if (parseFloat(this.state.amount)) {
+                    this.props.addItem(this.props.desc, parseFloat(this.state.amount), this.props.essential);
+                    this.setState({amount: ''});
+                    this.nextView();
+                  }
+                }}>
+                <CurrencyInput value={this.state.amount} onChange={this.handleAmountChange} />
+                <button type="submit" className="btn btn-success" style={{margin: '10px'}}>Add Item</button>
+                <button type="button" className="btn btn-default" onClick={this.nextView}>Skip Item</button>
+              </form>
+          </div>
+        </TransparentContainer>
       </div>
     )
   }
