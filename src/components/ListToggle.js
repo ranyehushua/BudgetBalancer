481c38ff24845bc35ad5795ad148e5350700c341
ranyehushua <ranyehushua@gmail.com>
2017-04-04 12:13:11 -0600

    ListToggle now functional

--- src/components/ListToggle.js
+++ src/components/ListToggle.js
@@ -4,32 +4,49 @@ const ListToggle = React.createClass({
   propTypes: {
     handleToggle: PropTypes.func.isRequired
   },
+  styles: {
+    button: {
+      margin: '0px 10px',
+      width: '100px'
+    }
+  },
+  getInitialState() {
+    return {selected: 'INCOME'}
+  },
   toggleIncome() {
     this.props.handleToggle('INCOME');
-    this.className = "btn btn-info"
+    this.setState({selected: 'INCOME'});
   },
   toggleExpenses() {
     this.props.handleToggle('EXPENSES');
+    this.setState({selected: 'EXPENSES'});
   },
   toggleSavings() {
     this.props.handleToggle('SAVINGS');
-  },
-  toggleAll() {
-    this.props.handleToggle('ALL');
+    this.setState({selected: 'SAVINGS'});
   },
   render () {
     return (
       <div className="form-group text-center">
-        <button type="button" className="btn btn-default" onClick={this.toggleIncome}>Income</button>
-        <button type="button" className="btn btn-default" onClick={this.toggleExpenses}>Expenses</button>
-        <button type="button" className="btn btn-default" onClick={this.toggleSavings}>Savings</button>
-        <button type="button" className="btn btn-default" onClick={this.toggleAll}>All</button>
+        <button type="button"
+                className={this.state.selected === 'INCOME' ? "btn btn-info" : "btn btn-default"}
+                onClick={this.toggleIncome} style={this.styles.button}
+                >Income
+        </button>
+        <button type="button"
+                className={this.state.selected === 'EXPENSES' ? "btn btn-info" : "btn btn-default"}
+                onClick={this.toggleExpenses} style={this.styles.button}
+                >Expenses
+        </button>
+        <button type="button"
+                className={this.state.selected === 'SAVINGS' ? "btn btn-info" : "btn btn-default"}
+                onClick={this.toggleSavings} style={this.styles.button}
+                >Savings
+        </button>
       </div>
     )
   }
-})
-
-
+});
 
 
 export default ListToggle;
