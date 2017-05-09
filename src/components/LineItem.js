3943282e3ad6393e3d61b35c306cb1199eabcd05
ranyehushua <ranyehushua@gmail.com>
2017-05-09 12:39:42 -0600

    Updated line item styling for better alignment

--- src/components/LineItem.js
+++ src/components/LineItem.js
@@ -10,14 +10,21 @@ const style={
     color: '#fff'
   },
   button: {
-    padding: '1px 3px',
+    padding: '5px 2% 2px',
     float: 'right',
     fontSize: '12px',
-    marginLeft: '3px'
+    marginLeft: '5px',
+    display: 'inline',
+    verticalAlign: 'middle',
+    lineHeight: '1.8rem'
   },
   desc: {
     float: 'left',
-    maxWidth: '75%'
+    maxWidth: '75%',
+    fontSize: '1.1rem',
+    lineHeight: '2.2em',
+    display: 'inline',
+    verticalAlign: 'middle'
   },
   clear: {
     clear: 'both'
@@ -48,7 +55,7 @@ const LineItem = React.createClass({
       <li style={style.li}>
         {this.state.editting === true
           ? <EditLineItem edit={this.submitEdit} id={this.props.id} desc={this.props.desc} amount={this.props.amount} />
-          : <div>
+        : <div style={{lineHeight: '2rem'}}>
               <div style={style.desc}>{this.props.desc + ": $" + this.props.amount}</div>
               <button className="btn btn-danger" onClick={this.props.remove} style={style.button}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
               <button className="btn btn-default" onClick={() => {this.setState({editting: !this.state.editting})}} style={style.button}><span className="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
