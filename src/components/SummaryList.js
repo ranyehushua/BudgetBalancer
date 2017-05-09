ef1facb43924f9c74b86be73e0c94082b31be976
ranyehushua <ranyehushua@gmail.com>
2017-05-09 15:18:06 -0600

    Added heading to summary list

--- src/components/SummaryList.js
+++ src/components/SummaryList.js
@@ -4,18 +4,21 @@ import SummaryLI from './SummaryLI'
 
 const SummaryList = (props) => {
   return (
-    <ul>
-      <SummaryLI desc="Monthly Income" amount={props.income.total} />
-      <SummaryLI desc="Monthly Essential Expenses" amount={props.expense.totalEssential} />
-      <SummaryLI desc="Monthly Discretionary Expenses" amount={props.expense.totalDiscretionary} />
-      <SummaryLI desc="Allocated Savings" amount={props.savings.total} />
-      {props.surplus > 0
-        ? <SummaryLI desc="Total Surplus" amount={props.surplus} surplus />
-        : props.surplus < 0
-          ? <SummaryLI desc="Total Deficit" amount={props.surplus * -1} deficit />
-          : null
-      }
-    </ul>
+    <div>
+      <h2 className="text-center">Monthly Budget Summary</h2>
+      <ul>
+        <SummaryLI desc="Monthly Income" amount={props.income.total} />
+        <SummaryLI desc="Monthly Essential Expenses" amount={props.expense.totalEssential} />
+        <SummaryLI desc="Monthly Discretionary Expenses" amount={props.expense.totalDiscretionary} />
+        <SummaryLI desc="Allocated Savings" amount={props.savings.total} />
+        {props.surplus > 0
+          ? <SummaryLI desc="Total Surplus" amount={props.surplus} surplus />
+          : props.surplus < 0
+            ? <SummaryLI desc="Total Deficit" amount={props.surplus * -1} deficit />
+            : null
+        }
+      </ul>
+    </div>
   )
 }
 
