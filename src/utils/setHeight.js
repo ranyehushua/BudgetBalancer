497157b40654175792f31883783b0e585bfb780d
ranyehushua <ranyehushua@gmail.com>
2017-05-15 23:57:12 -0600

    setHeight now scrolls up

--- src/utils/setHeight.js
+++ src/utils/setHeight.js
@@ -4,6 +4,7 @@ const setHeight = () => {
   //Using .last() because the last instance of app class will always be the Lists component
   if ($('html').height() < $('.app').last().height()) $('html').height($('.app').last().height());
   console.log('setting height');
+  $('body').scrollTop(0);
 }
 
 export default setHeight;
